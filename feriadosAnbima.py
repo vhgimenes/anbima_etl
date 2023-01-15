3732bf3850961a24430bd2e40b655a6c42c4c3fd
Victor Gimenes <108276273+vhgimenes@users.noreply.github.com>
2023-01-15 19:03:17 -0300

    Add files via upload

---
+++ feriadosAnbima.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 14:07:25 2021
+
+@author: victor.gimenes
+"""
+import xlrd
+from datetime import date
+from urllib.request import urlretrieve
+
+def holidays(url=None, path=None):
+    if not url:
+        url = 'http://www.anbima.com.br/feriados/arqs/feriados_nacionais.xls'
+    if not path:
+        path = 'feriados_nacionais.xls'
+    try:
+        wb = xlrd.open_workbook(path)
+    except:
+        response = urlretrieve(url, filename=path)
+        wb = xlrd.open_workbook(path)
+    ws = wb.sheet_by_index(0)
+    i = 1
+    dates = []
+    while ws.cell_type(i, 0) == 3:
+        y, m, d, _, _, _ = xlrd.xldate_as_tuple(ws.cell_value(i, 0), wb.datemode)
+        dates.append(date(y, m, d))
+        i += 1
+    return dates
